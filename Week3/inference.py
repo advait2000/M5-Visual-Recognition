6b6e411791154a55803884b56eb81aa3e7be9bdc
advait2000 <46642008+advait2000@users.noreply.github.com>
2023-03-22 16:21:31 +0100

    Task 1

---
+++ Week3/inference.py
@@ -0,0 +1,35 @@
+# Setup detectron2 logger
+from detectron2.utils.logger import setup_logger
+setup_logger()
+
+# Import required packages
+import cv2
+from imutils import paths
+from detectron2 import model_zoo
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.utils.visualizer import Visualizer
+from detectron2.data import MetadataCatalog
+
+# Initialize the configs and load model weights
+cfg = get_cfg()
+cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # set threshold for this model
+cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
+
+# Initialize the predictor
+predictor = DefaultPredictor(cfg)
+
+# Loop over the dataset
+for i, path in enumerate(sorted(paths.list_images("/Users/advaitdixit/Documents/Masters/dataset/out_of_context"))):
+    # Read image and pass through model
+    im = cv2.imread(path)
+    outputs = predictor(im)
+
+    # We can use `Visualizer` to draw the predictions on the image.
+    v = Visualizer(im[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2)
+    out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+
+    # Save the image
+    cv2.imwrite("output/{}.png".format(i), out.get_image()[:, :, ::-1])
+    print("Saved {}".format(path))
