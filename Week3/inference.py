4a174473948b946bf1ce2c2f000ca9eba85cf846
advait2000 <46642008+advait2000@users.noreply.github.com>
2023-03-25 15:24:44 +0100

    Argument parser added

--- Week3/inference.py
+++ Week3/inference.py
@@ -1,15 +1,21 @@
-# Setup detectron2 logger
-from detectron2.utils.logger import setup_logger
-setup_logger()
-
 # Import required packages
 import cv2
 from imutils import paths
+import argparse
 from detectron2 import model_zoo
 from detectron2.engine import DefaultPredictor
 from detectron2.config import get_cfg
 from detectron2.utils.visualizer import Visualizer
 from detectron2.data import MetadataCatalog
+
+# Setup detectron2 logger
+from detectron2.utils.logger import setup_logger
+setup_logger()
+
+# Construct argument parser and parse arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-d", "--dataset", required=True, help="Path to the images to inferred")
+args = vars(ap.parse_args())
 
 # Initialize the configs and load model weights
 cfg = get_cfg()
@@ -21,7 +27,7 @@ cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask
 predictor = DefaultPredictor(cfg)
 
 # Loop over the dataset
-for i, path in enumerate(sorted(paths.list_images("/Users/advaitdixit/Documents/Masters/dataset/out_of_context"))):
+for i, path in enumerate(sorted(paths.list_images(args["dataset"]))):
     # Read image and pass through model
     im = cv2.imread(path)
     outputs = predictor(im)
@@ -31,5 +37,5 @@ for i, path in enumerate(sorted(paths.list_images("/Users/advaitdixit/Documents/
     out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
 
     # Save the image
-    cv2.imwrite("output/{}.png".format(i), out.get_image()[:, :, ::-1])
+    cv2.imwrite("output{}.png".format(i), out.get_image()[:, :, ::-1])
     print("Saved {}".format(path))
