b6c37c0578e3f3719f8d8f9310acb5e53529c407
Hogea Eduard-Florin <eduard.hogea00@e-uvt.ro>
2023-04-13 22:53:41 +0300

    added ROC curve

--- Week4/knn.py
+++ Week4/knn.py
@@ -1,7 +1,11 @@
 # Import required packages
 import os
 import pickle
+import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import precision_recall_curve
+from sklearn.preprocessing import label_binarize
+import matplotlib.pyplot as plt
 from average_precision import *
 
 # Load the features for the dataset and queries
@@ -43,3 +47,26 @@ print('P@1=', p_1)
 print('P@5=', p_5)
 map = mAP(ground_truth, predictions)
 print('mAP=', map)
+# Convert ground truth and predictions to binary format
+num_classes = len(set(labels))
+binary_ground_truth = label_binarize(ground_truth, classes=range(num_classes))
+binary_predictions = []
+
+for pred in predictions:
+    binary_pred = label_binarize(pred, classes=range(num_classes))
+    binary_predictions.append(binary_pred.mean(axis=0))
+
+binary_predictions = np.array(binary_predictions)
+
+# Calculate precision-recall curve
+precision, recall, _ = precision_recall_curve(binary_ground_truth.ravel(), binary_predictions.ravel())
+
+# Plot precision-recall curve
+plt.figure()
+plt.plot(recall, precision, marker='.', label='KNN Classifier')
+plt.xlabel('Recall')
+plt.ylabel('Precision')
+plt.title('Precision-Recall Curve')
+plt.legend(loc='best')
+plt.grid()
+plt.savefig("Week4/roc.png")
